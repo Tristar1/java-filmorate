22be19e1a5f24c6d276ac9989aebbff44352877d
Tristar1 <106386060+Tristar1@users.noreply.github.com>
2022-11-30 13:33:01 +0300

    Sprint 9

--- src/main/java/ru/yandex/practicum/filmorate/controllers/ValidationFilmException.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/ValidationFilmException.java
@@ -13,19 +13,3 @@ public class ValidationFilmException extends Exception {
     }
 
 }
-=======
-package ru.yandex.practicum.filmorate.controllers;
-
-import org.springframework.http.HttpStatus;
-
-public class ValidationFilmException extends Exception {
-
-    HttpStatus httpStatus;
-
-    public ValidationFilmException(final String message, HttpStatus httpStatus) {
-        super(message);
-        this.httpStatus = httpStatus == null ? HttpStatus.BAD_REQUEST : httpStatus;
-    }
-
-}
-
