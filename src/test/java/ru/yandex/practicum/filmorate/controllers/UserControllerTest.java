44db160e8f26033fdaa9836f7e723d39d7a1b22d
Tristar1 <106386060+Tristar1@users.noreply.github.com>
2022-11-30 18:07:27 +0300

    #3 Sprint 9

---
+++ src/test/java/ru/yandex/practicum/filmorate/controllers/UserControllerTest.java
@@ -0,0 +1,55 @@
+package ru.yandex.practicum.filmorate.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    private final UserController userController = new UserController();
+
+    @Test
+    void validate() {
+
+        User user = User.builder()
+                .id(1)
+                .birthday(LocalDate.now().minusDays(50))
+                .email("Serg@mail.ru")
+                .login("Serg")
+                .build();
+
+        //Тест почты
+        user.setEmail("");
+
+        ValidationUserException exception = assertThrows(ValidationUserException.class,generateExecutable(user));
+        assertEquals("Почта не может быть пустой и должна включать символ @", exception.getMessage());
+
+        user.setEmail("Serg.ru");
+
+        exception = assertThrows(ValidationUserException.class,generateExecutable(user));
+        assertEquals("Почта не может быть пустой и должна включать символ @", exception.getMessage());
+
+        // Тест логина
+
+        user.setEmail("Serg@mail.ru");
+        user.setLogin("f f");
+        exception = assertThrows(ValidationUserException.class,generateExecutable(user));
+        assertEquals("Логин не может быть пустым и содержать пробелы!", exception.getMessage());
+
+        // Тест даты рождения
+        user.setLogin("Serg");
+        user.setBirthday(LocalDate.now().plusDays(1));
+
+        exception = assertThrows(ValidationUserException.class,generateExecutable(user));
+        assertEquals("Дата рождения пользователя не может быть больше текущей!", exception.getMessage());
+
+    }
+
+    private Executable generateExecutable(User user) {
+        return () -> userController.validateUser(user);
+    }
+}
